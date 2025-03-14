2910eab06e4399ae123eb9f5b1eb8bd500eeed19
Ron Appleton <ron.appleton@gmail.com>
2025-03-14 21:44:04 +0000

    Add Service Provider and dependencies

---
+++ src/TicketPickerServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppEngine\TicketPicker;
+
+use Illuminate\Support\ServiceProvider;
+
+class TicketPickerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+    }
+}
